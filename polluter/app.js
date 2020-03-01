6ac7ed465e578573a24c866026b54b90653baf41
0xEther <60259703+0xEther@users.noreply.github.com>
2020-03-01 10:23:38 +0000

    Update app.js

--- polluter/app.js
+++ polluter/app.js
@@ -1,15 +1,15 @@
 "use strict";
-var __awaiter = undefined && undefined.__awaiter || function (o, a, s, u) {
+var __awaiter = undefined && undefined.__awaiter || function (o, a, s, l) {
   return new (s || (s = Promise))(function (e, t) {
     function r(e) {
       try {
-        i(u.next(e));
+        i(l.next(e));
       } catch (e) {
         t(e);
       }
     }function n(e) {
       try {
-        i(u.throw(e));
+        i(l.throw(e));
       } catch (e) {
         t(e);
       }
@@ -17,7 +17,7 @@ var __awaiter = undefined && undefined.__awaiter || function (o, a, s, u) {
       t.done ? e(t.value) : new s(function (e) {
         e(t.value);
       }).then(r, n);
-    }i((u = u.apply(o, a || [])).next());
+    }i((l = l.apply(o, a || [])).next());
   });
 },
     __generator = undefined && undefined.__generator || function (r, n) {
@@ -63,24 +63,24 @@ var __awaiter = undefined && undefined.__awaiter || function (o, a, s, u) {
     web3 = window.web3 || void 0,
     utils = ethers_1.ethers.utils,
     provider = new ethers_1.ethers.providers.Web3Provider(web3.currentProvider),
-    signer = provider.getSigner();jQuery(function (l) {
+    signer = provider.getSigner();jQuery(function (c) {
   return __awaiter(_this, void 0, void 0, function () {
     var u;return __generator(this, function (e) {
-      return l(".js--connect").click(function (t) {
+      return c(".js--connect").click(function (t) {
         return __awaiter(this, void 0, void 0, function () {
           return __generator(this, function (e) {
             switch (e.label) {case 0:
                 return t.preventDefault(), u ? (alert("already enabled"), [2]) : [4, window.ethereum.enable()];case 1:
-                return (u = e.sent()) && l(this).text("Wallet Connected"), [2];}
+                return (u = e.sent()) && c(this).text("Wallet Connected"), [2];}
           });
         });
-      }), l(".js--pollute").click(function (s) {
+      }), c(".js--pollute").click(function (l) {
         return __awaiter(this, void 0, void 0, function () {
-          var t, r, n, i, o, a;return __generator(this, function (e) {
+          var t, r, n, i, o, a, s;return __generator(this, function (e) {
             switch (e.label) {case 0:
-                return s.preventDefault(), u ? (l(this).addClass("polluting"), [4, new ethers_1.ethers.Contract("0x57AaFA7eA3D66e2C9540d455BDBe093De9DB6bf4", polluterAbi, signer).iterator()]) : (alert("connect wallet"), [2]);case 1:
-                return t = e.sent(), l(this).text("Polluting"), [4, t.wait(1)];case 2:
-                return r = e.sent(), l(this).text("Pollute"), l(this).removeClass("polluting"), n = r.events, i = n[n.length - 1], o = i.args.totalGasOffset, a = i.args.methodFootprint, (o || a) && l("pre").append("totalGasOffset: " + o + "<br />\n                methodFootprint: " + a), [2];}
+                return l.preventDefault(), u ? (c(this).addClass("polluting"), [4, new ethers_1.ethers.Contract("0x57AaFA7eA3D66e2C9540d455BDBe093De9DB6bf4", polluterAbi, signer).iterator()]) : (alert("connect wallet"), [2]);case 1:
+                return t = e.sent(), c(this).text("Polluting"), [4, t.wait(1)];case 2:
+                return r = e.sent(), c(this).text("Pollute"), c(this).removeClass("polluting"), n = r.events, i = n[n.length - 1], o = i.args.totalGasOffset, a = i.args.methodFootprint, (o || a) && (s = c(".polluter__spin"), c(s[0]).find("img").attr("src", "./images/3.png"), c(s[1]).find("img").attr("src", "./images/4.png"), c("body").addClass("clean"), c("pre").append("totalGasOffset: " + o + "<br />\n                methodFootprint: " + a), c(this).hide()), [2];}
           });
         });
       }), [2];
