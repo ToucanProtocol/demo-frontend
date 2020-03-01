0323b5e2ebf3ead2d381d1082e323849aa4c44c4
Adam Spiers <git@adamspiers.org>
2020-03-01 04:24:03 +0000

    final blah

--- webflow/src/dapp.ts
+++ webflow/src/dapp.ts
@@ -1,20 +1,21 @@
 // import "babel-polyfill";
+import * as ethco2 from "../../../eth-co2/dist/ts/eth-co2";
 import { ethers } from "ethers";
-import { jQuery } from "jquery";
 
+let jQuery = (<any>window).jQuery || undefined;
 let $ = jQuery;
 
-// import { getCo2kenPrice } from "../../eth-co2/dist/ts/eth-co2";
-
-const dapp = async () => {
-
-//   let provider = new ethers.providers.Web3Provider(web3.currentProvider);
-//   window.price = await getCo2kenPrice(provider);
-
-  $("#tonnes-co2").text("12342");
-
-};
 
 jQuery(async ($) => {
-  dapp().catch(e => { console.error(e) });
+  const provider = new ethers.providers.Web3Provider((<any>window).web3.currentProvider);
+  (<any>window).foo = provider;
+  //const signer = provider.getSigner();
+  let price = await ethco2.getCo2kenPrice(provider);
+  (<any>window).price = price;
+
+  $("#tonnes-co2").on('input', function() {
+    let tonnes = $(this).val();
+    $("#offset-dai").val(price * tonnes);
+    $("#offset-payment").val(price * tonnes * 1e18);
+  });
 });
