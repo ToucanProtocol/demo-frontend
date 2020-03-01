32d10c98cdcd2e73f82642fea085a29ec24f46bd
Adam Spiers <git@adamspiers.org>
2020-03-01 03:24:36 +0000

    temporarily disable library

--- webflow/src/dapp.ts
+++ webflow/src/dapp.ts
@@ -1,11 +1,11 @@
 import "babel-polyfill";
 import { ethers } from "ethers";
 
-import { getCo2kenPrice } from "../../eth-co2/dist/ts/eth-co2";
+// import { getCo2kenPrice } from "../../eth-co2/dist/ts/eth-co2";
 
-const dapp = async () => {
-  let provider = new ethers.providers.Web3Provider(web3.currentProvider);
-  window.price = await getCo2kenPrice(provider);
-};
+// const dapp = async () => {
+//   let provider = new ethers.providers.Web3Provider(web3.currentProvider);
+//   window.price = await getCo2kenPrice(provider);
+// };
 
 dapp().catch(e => { console.error(e) });
