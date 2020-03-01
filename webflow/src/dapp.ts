29b495489e4f633ebe296dfb9b58075f778a53f7
Adam Spiers <git@adamspiers.org>
2020-03-01 03:36:25 +0000

    try to setup jQuery

--- webflow/src/dapp.ts
+++ webflow/src/dapp.ts
@@ -1,5 +1,8 @@
-import "babel-polyfill";
+// import "babel-polyfill";
 import { ethers } from "ethers";
+import { jQuery } from "jquery";
+
+let $ = jQuery;
 
 // import { getCo2kenPrice } from "../../eth-co2/dist/ts/eth-co2";
 
@@ -8,6 +11,10 @@ const dapp = async () => {
 //   let provider = new ethers.providers.Web3Provider(web3.currentProvider);
 //   window.price = await getCo2kenPrice(provider);
 
+  $("#tonnes-co2").text("12342");
+
 };
 
-dapp().catch(e => { console.error(e) });
+jQuery(async ($) => {
+  dapp().catch(e => { console.error(e) });
+});
