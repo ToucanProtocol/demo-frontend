d4d65f11cd92a9285af8216a71d8118386e71201
Adam Spiers <blockchain@adamspiers.org>
2020-03-26 21:37:28 +0000

    Make dappHero realise when #offset-payment input field is updated

--- webflow/src/dapp.ts
+++ webflow/src/dapp.ts
@@ -5,6 +5,11 @@ import { ethers } from "ethers";
 let jQuery = (<any>window).jQuery || undefined;
 let $ = jQuery;
 
+function triggerChangeOnElement(selector) {
+  const elem = document.querySelector(selector);
+  const event = new Event('input');
+  elem.dispatchEvent(event);
+}
 
 jQuery(async ($) => {
   const provider = new ethers.providers.Web3Provider((<any>window).web3.currentProvider);
@@ -17,5 +22,6 @@ jQuery(async ($) => {
     let tonnes = $(this).val();
     $("#offset-dai").val(price * tonnes);
     $("#offset-payment").val(price * tonnes * 1e18);
+    triggerChangeOnElement("#offset-payment");
   });
 });
