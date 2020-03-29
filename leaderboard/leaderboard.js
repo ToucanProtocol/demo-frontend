db7af3df995b3863c17b6bec981b901bd83fe169
Raphael Benoit <raphmc@hotmail.com>
2020-03-29 20:51:23 +0200

    custom JS for leaderboard

---
+++ leaderboard/leaderboard.js
@@ -0,0 +1,29 @@
+// <script src="https://raw.githubusercontent.com/CO2ken/demo-frontend/master/leaderboard/index.js"></script>
+
+$(document).ready(function () {
+
+    let leaderboardPlace = "...";
+    let retiredCO2ken = "...";
+    let totalDaiSpent = "...";
+
+    $("#leaderboard-place").text(leaderboardPlace);
+    $("#total-retired-co2ken").text(retiredCO2ken);
+    $("#total-dai-spent").text(totalDaiSpent);
+
+    $("#eth-address-search").click(function () {
+        let address = $("#eth-address-search").val();
+
+        $.ajax({
+        url: "https://api.thegraph.com/subgraphs/name/benesjan/co2ken",
+        contentType: "application/json", type: 'POST',
+        data: JSON.stringify({
+            query: `{ userBalances(id: \"${address}\") { balance } }`
+        }),
+        success: function (result) {
+            let retiredCO2kenDecimals = result['data']['userBalances'][0]['balance'];
+            retiredCO2ken = retiredCO2kenDecimals / 1e18;
+            $("#total-retired-co2ken").text(retiredCO2ken);
+        }
+        });
+    });
+});
