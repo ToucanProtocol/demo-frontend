65c441f6af459095ff8cf66ae94776a4cf6e064f
Adam Spiers <blockchain@adamspiers.org>
2020-03-30 00:12:46 +0100

    fix indentation

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -2,36 +2,36 @@
 
 $(document).ready(function () {
 
-    let ethAddress;
-    
-    setTimeout( function() {
-        ethAddress = $("#eth-user-address").val();
-	    console.log(ethAddress);
-        $("#current-eth-address").val(ethAddress);
-    }, 2000);
-    
-    let leaderboardPlace = "...";
-    let retiredCO2ken = "...";
-    let totalDaiSpent = "...";
+  let ethAddress;
 
-    $("#leaderboard-place").text(leaderboardPlace);
-    $("#total-retired-co2ken").text(retiredCO2ken);
-    $("#total-dai-spent").text(totalDaiSpent);
+  setTimeout( function() {
+    ethAddress = $("#eth-user-address").val();
+    console.log(ethAddress);
+    $("#current-eth-address").val(ethAddress);
+  }, 2000);
 
-    $("#eth-address-search").click(function () {
-        let address = $("#eth-address-search").val();
+  let leaderboardPlace = "...";
+  let retiredCO2ken = "...";
+  let totalDaiSpent = "...";
 
-        $.ajax({
-        url: "https://api.thegraph.com/subgraphs/name/benesjan/co2ken",
-        contentType: "application/json", type: 'POST',
-        data: JSON.stringify({
-            query: `{ userBalances(id: \"${address}\") { balance } }`
-        }),
-        success: function (result) {
-            let retiredCO2kenDecimals = result['data']['userBalances'][0]['balance'];
-            retiredCO2ken = retiredCO2kenDecimals / 1e18;
-            $("#total-retired-co2ken").text(retiredCO2ken);
-        }
-        });
+  $("#leaderboard-place").text(leaderboardPlace);
+  $("#total-retired-co2ken").text(retiredCO2ken);
+  $("#total-dai-spent").text(totalDaiSpent);
+
+  $("#eth-address-search").click(function () {
+    let address = $("#eth-address-search").val();
+
+    $.ajax({
+      url: "https://api.thegraph.com/subgraphs/name/benesjan/co2ken",
+      contentType: "application/json", type: 'POST',
+      data: JSON.stringify({
+        query: `{ userBalances(id: \"${address}\") { balance } }`
+      }),
+      success: function (result) {
+        let retiredCO2kenDecimals = result['data']['userBalances'][0]['balance'];
+        retiredCO2ken = retiredCO2kenDecimals / 1e18;
+        $("#total-retired-co2ken").text(retiredCO2ken);
+      }
     });
+  });
 });
