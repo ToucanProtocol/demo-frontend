87930e237f1847e5e64d5b9fd8986e8491ddf9f6
Adam Spiers <blockchain@adamspiers.org>
2020-03-31 01:54:52 +0100

    Add contract totals to leaderboard.{html,js}

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -18,9 +18,36 @@ function showUserResults(address) {
       let retiredCO2kenDecimals = userBalance['balance'];
       retiredCO2ken = retiredCO2kenDecimals / 1e18;
       console.log("Retired CO2ken:", retiredCO2kenDecimals);
-      $("#total-retired-co2ken").text(retiredCO2ken);
+      $("#user-retired-co2kens").text(retiredCO2ken);
     }
   });
+}
+
+function getContractTotals() {
+  let contractAddress = "0x93ec2167da2a83fbbe61567f67f71750c13b9c09";
+
+  // Request for the total number of offsetted tokens
+  let query = `{ contractBalance(id: "${contractAddress}") { offsetted daiReceived } }`;
+  console.log(query);
+  $.ajax({
+    url: "https://api.thegraph.com/subgraphs/name/benesjan/co2ken",
+    contentType: "application/json", type: 'POST',
+    data: JSON.stringify({
+      query: query
+    }),
+    success: function (result) {
+      console.log("contractBalance", result);
+      let contractBalance = result['data']['contractBalance']
+      if (!contractBalance) {
+        console.warn("Failed to get totals for contract");
+        return;
+      }
+      let offset = contractBalance['offsetted'] / 1e18;
+      let daiReceived = contractBalance["daiReceived"] / 1e18;
+      $("#contract-retired-co2kens").text(offset);
+      $("#contract-dai-received").text(daiReceived);
+    }
+  })
 }
 
 $(document).ready(function () {
@@ -28,9 +55,11 @@ $(document).ready(function () {
   let retiredCO2ken = "...";
   let totalDaiSpent = "...";
 
-  $("#leaderboard-place").text(leaderboardPlace);
-  $("#total-retired-co2ken").text(retiredCO2ken);
-  $("#total-dai-spent").text(totalDaiSpent);
+  $("#leaderboard-rank").text(1);
+  $("#user-retired-co2kens").text(retiredCO2ken);
+  $("#user-dai-spent").text(totalDaiSpent);
+
+  getContractTotals();
 
   window.addEventListener('dappHeroConfigLoaded', ({ detail: dappHero }) => {
     if (dappHero.provider) {
