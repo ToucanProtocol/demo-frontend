a600452653dbe510cb654034febac9ed32c41c64
Adam Spiers <blockchain@adamspiers.org>
2020-03-31 01:27:43 +0100

    Make showUserResults() more robust

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -2,14 +2,20 @@
 
 function showUserResults(address) {
   console.log(`Getting results for ${address}`);
+  let query = `{ userBalance(id: \"${address.toLowerCase()}\") { id balance } }`;
+  console.log(query);
   $.ajax({
     url: "https://api.thegraph.com/subgraphs/name/benesjan/co2ken",
     contentType: "application/json", type: 'POST',
-    data: JSON.stringify({
-      query: `{ userBalances(id: \"${address}\") { balance } }`
-    }),
+    data: JSON.stringify({ query: query }),
     success: function (result) {
-      let retiredCO2kenDecimals = result['data']['userBalances'][0]['balance'];
+      console.log(result);
+      let userBalance = result['data']['userBalance'];
+      if (!userBalance) {
+        console.log(`No events found for user ${address}`);
+        return;
+      }
+      let retiredCO2kenDecimals = userBalance['balance'];
       retiredCO2ken = retiredCO2kenDecimals / 1e18;
       console.log("Retired CO2ken:", retiredCO2kenDecimals);
       $("#total-retired-co2ken").text(retiredCO2ken);
